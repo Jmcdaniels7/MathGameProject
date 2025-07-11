4b78d0eff7cb5e98a614e0016a42ac91aec00f65
Jamcda <jamcdaniels99@gmail.com>
2025-07-11 09:33:15 -0400

    first commit, project initialized with gitignore file

---
+++ mathgame/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# we need to make a math game that teaches addition, subraction, 
+# multiplication, division, negative numbers, PEMDAS, and fractions
+# this needs to be game like, we need a meter for xp and level.
+# each type of math needs to be its own story
+# utilize blue, green, white, and yellow for happy and calming colors
+# we need to utilize a mobile development framework Swift or Kotlin
